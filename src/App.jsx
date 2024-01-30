15222a7a4159a99e530a33a8bbbc964b273eb77c
nguyxnhung20 <nguyenduyhung.20052002@gmail.com>
2024-01-30 20:19:15 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
-import { useState } from 'react';
-import './App.css';
-import Result from "./components/Result.jsx";
+import './App.css'
+import ProductForm from "./components/ProductForm.jsx";
+import {useState} from "react";
 
 function App() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [isSubmitted, setSubmitted] = useState(false);
 
-    function submit() {
-        console.log(name, email, password, confirmPassword);
-        setSubmitted(true);
-    }
+  const [products, setProducts] = useState([])
 
-    function changeName(e) {
-        setName(e.target.value);
-        setSubmitted(false);
-    }
+  function saveProduct(newProduct) {
+    setProducts([...products, newProduct])
+  }
 
-    function changeEmail(e) {
-        setEmail(e.target.value);
-        setSubmitted(false);
-    }
+  return (
+    <div className="container">
+      <ProductForm saveProduct={saveProduct}/>
+      <p>Danh sách sản phẩm</p>
+      <table>
+        <thead>
+        <tr>
+          <th>
+            Tên
+          </th>
+          <th>
+            Nhãn hàng
+          </th>
+          <th>
+            Số lượng
+          </th>
+          <th>
+            Giá tiền
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          products.map(item => <tr>
+            <td>
+              {item.name}
+            </td>
+            <td>
+              {item.company}
+            </td>
+            <td>
+              {item.amount}
+            </td>
+            <td>
+              {item.price}
+            </td>
+          </tr>)
+        }
+        </tbody>
+      </table>
 
-    function changePassword(e) {
-        setPassword(e.target.value);
-        setSubmitted(false);
-    }
-
-    function changeConfirmPassword(e) {
-        setConfirmPassword(e.target.value);
-        setSubmitted(false);
-    }
-
-    return (
-        <div className="background">
-            <div className="login-form">
-                <p className="title">Register</p>
-                <input className="input" placeholder="Name" onChange={changeName} />
-                <input className="input" placeholder="Email Address" onChange={changeEmail} />
-                <input className="input" placeholder="Password" onChange={changePassword} />
-                <input className="input" placeholder="Confirm Password" onChange={changeConfirmPassword} />
-                <button className="login-submit" onClick={submit}>Register</button>
-                <p className="not-a-member">Already a member? <span className="sign-sp">Login now</span></p>
-            </div>
-            {
-                isSubmitted ? <Result name={name} email={email} password={password} confirmPassword={confirmPassword} /> : null
-            }
-
-        </div>
-    );
+    </div>
+  )
 }
 
-export default App;
+export default App
